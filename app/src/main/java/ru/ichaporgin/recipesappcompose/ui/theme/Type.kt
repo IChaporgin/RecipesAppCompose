3302101de59c82db99922882f795499ba1d8ecf2
Igor Chaporgin <fellclub@yandex.ru>
2025-08-11 23:50:56 +0300

    RCA-05 add custom font

--- app/src/main/java/ru/ichaporgin/recipesappcompose/ui/theme/Type.kt
+++ app/src/main/java/ru/ichaporgin/recipesappcompose/ui/theme/Type.kt
@@ -32,3 +32,29 @@ val Typography = Typography(
     )
     */
 )
+
+val recipesAppTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = montserratAlternatesFontFamily,
+        fontSize = 20.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = montserratAlternatesFontFamily,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+)
