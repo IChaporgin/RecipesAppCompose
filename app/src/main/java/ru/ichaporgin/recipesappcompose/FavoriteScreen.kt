daa63d175cd2d8d67b72d633c379fb5df08d1874
Igor Chaporgin <fellclub@yandex.ru>
2025-08-25 13:16:06 +0300

    RCA-09 Подготовлен элемент CategoryItem.kt, добавлена тестовая сетка категорий

--- app/src/main/java/ru/ichaporgin/recipesappcompose/FavoriteScreen.kt
+++ app/src/main/java/ru/ichaporgin/recipesappcompose/FavoriteScreen.kt
@@ -3,7 +3,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ru.ichaporgin.recipesappcompose.R
--- app/src/main/java/ru/ichaporgin/recipesappcompose/ui/categories/CategoriesScreen.kt
+++ app/src/main/java/ru/ichaporgin/recipesappcompose/ui/categories/CategoriesScreen.kt
@@ -1,33 +1,58 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.ichaporgin.recipesappcompose.R
 import ru.ichaporgin.recipesappcompose.ui.ScreenHeader
+import ru.ichaporgin.recipesappcompose.ui.categories.CategoryItem
 
 @Composable
-fun Categories(){
+fun Categories() {
     val categoriesTitle = "Категории"
     val categoriesImage = R.drawable.bcg_categories
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(MaterialTheme.colorScheme.background),
+    val categoryItemTitle = "Бургеры"
+    val categoryItemDescription = "Рецепты всех популярных бургеров"
+    val categoryItemImage = R.drawable.burger
+
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.background),
         ) {
-        ScreenHeader(categoriesTitle,
-            categoriesImage,)
+            ScreenHeader(
+                categoriesTitle,
+                categoriesImage,
+            )
+        }
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            horizontalArrangement = androidx.compose.foundation.layout.Arrangement.spacedBy(16.dp),
+            verticalArrangement = androidx.compose.foundation.layout.Arrangement.spacedBy(16.dp)
+        ) {
+            items(10) {
+                CategoryItem(categoryItemTitle, categoryItemDescription, categoryItemImage)
+            }
+        }
     }
 }
 
 @Preview(
     showBackground = true,
-    showSystemUi = true
+    showSystemUi = true,
 )
 @Composable
-fun CategoriesPreview(){
+fun CategoriesPreview() {
     Categories()
 }
