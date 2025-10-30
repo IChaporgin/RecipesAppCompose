9369139d83b5067fa19d0ccc1ab66e2412c3511c
Igor Chaporgin <fellclub@yandex.ru>
2025-10-30 14:57:45 +0300

    RCA-17 подключил модуль kotlin-parcelize и добавил анотацию Parcelize RecipeUiModel, IngredientUiModel

--- app/src/main/java/ru/ichaporgin/recipesappcompose/core/ui/model/IngredientUiModel.kt
+++ app/src/main/java/ru/ichaporgin/recipesappcompose/core/ui/model/IngredientUiModel.kt
@@ -1,13 +1,16 @@
 package ru.ichaporgin.recipesappcompose.core.ui.model
 
+import android.os.Parcelable
 import androidx.compose.runtime.Immutable
+import kotlinx.parcelize.Parcelize
 import ru.ichaporgin.recipesappcompose.data.model.IngredientDto
 
 @Immutable
+@Parcelize
 data class IngredientUiModel(
     val name: String,
     val amount: String
-)
+) : Parcelable
 
 fun IngredientDto.toUiModel() = IngredientUiModel(
     name = description,
--- app/src/main/java/ru/ichaporgin/recipesappcompose/core/ui/model/RecipeUiModel.kt
+++ app/src/main/java/ru/ichaporgin/recipesappcompose/core/ui/model/RecipeUiModel.kt
@@ -1,23 +1,26 @@
 package ru.ichaporgin.recipesappcompose.core.ui.model
 
+import android.os.Parcelable
 import androidx.compose.runtime.Immutable
+import kotlinx.parcelize.Parcelize
 import ru.ichaporgin.recipesappcompose.data.model.IngredientDto
 import ru.ichaporgin.recipesappcompose.data.model.RecipeDto
 
 @Immutable
+@Parcelize
 data class RecipeUiModel(
     val id: Int,
     val title: String,
     val imageUrl: String,
-    val ingredients: List<IngredientDto>,
+    val ingredients: List<IngredientUiModel>,
     val method: List<String>,
     val isFavorite: Boolean = false
-)
+) : Parcelable
 
 fun RecipeDto.toUiModel() = RecipeUiModel(
     id = id,
     title = title,
     imageUrl = imageUrl,
-    ingredients = ingredients,
+    ingredients = ingredients.map { it.toUiModel() },
     method = method
 )
