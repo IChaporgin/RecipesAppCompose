8aecaac77f6492a836be174c922544f724c4dcc1
Igor Chaporgin <fellclub@yandex.ru>
2025-11-13 10:43:00 +0300

    RCA-19 настройка параметров в manifest

--- app/src/main/java/ru/ichaporgin/recipesappcompose/features/ingredient/ui/IngredientItem.kt
+++ app/src/main/java/ru/ichaporgin/recipesappcompose/features/ingredient/ui/IngredientItem.kt
@@ -3,7 +3,6 @@ package ru.ichaporgin.recipesappcompose.features.ingredient.ui
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
